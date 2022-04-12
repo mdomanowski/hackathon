42941718e6367ebffee139abaf71046427060b55
Michał Domanowski <michal.domanowski@ppuc.pl>
2022-04-12 14:39:49 +0200

    initial commit

---
+++ src/main/java/pl/hackathon/knowx/KnowxApplication.java
@@ -0,0 +1,13 @@
+package pl.hackathon.knowx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowxApplication.class, args);
+	}
+
+}
